ffdc19ecc8f8858ebb62ba2bf5312b6ecb1283c6
AsokTamang <ashoktmg2058@gmail.com>
2026-04-04 00:28:30 -0400

    data ingestion completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from src.exception import CustomError
+from src.logger import logging
+from dataclasses import dataclass
+import pandas as pd
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('root_data','train_data.csv')
+    test_data_path:str = os.path.join('root_data','test_data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            logging.info('Data Ingestion method started')
+            #reading the data from source
+            df_train = pd.read_csv(os.path.join('notebook/data','train.csv'))
+            df_test = pd.read_csv(os.path.join('notebook/data','test.csv'))
+            logging.info('Training and test Data read successfully')
+
+            #creating root data folder if not exist
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)  #creating the root data folder if the root data folder doesnot exist  
+
+            #storing the data in train and test path
+            df_train.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            df_test.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info('Data ingestion completed')
+
+        except Exception as e:
+            raise CustomError(e,sys)
--- src/exception.py
+++ src/exception.py
@@ -4,8 +4,8 @@ from src.logger import logging
 
 def error_message(message,error_detail:sys):
     _,_,exec_tb = error_detail.exc_info()
-    file_name = exec_tb.tb_frame.f_code.co_filename
-    line_number = exec_tb.tb_lineno
+    file_name = exec_tb.tb_frame.f_code.co_filename #extracting the file name where the error occured
+    line_number = exec_tb.tb_lineno  #extracting the line number where the error occured
     error_message = f'{message} error occured in {file_name} at line {line_number}'
     return error_message
 
