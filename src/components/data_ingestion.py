4e576c8bcf8d5eaca047119aa0eef6b1ee85f252
AsokTamang <ashoktmg2058@gmail.com>
2026-04-04 00:34:31 -0400

    files added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -33,3 +33,8 @@ class DataIngestion:
 
         except Exception as e:
             raise CustomError(e,sys)
+
+
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
